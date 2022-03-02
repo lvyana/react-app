4f259b5bd1fa62e34cf9cb0bcc12be6d7372c254
吕炎 <1376237693@qq.com>
2022-03-03 00:54:36 +0800

    添加插件type

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="react-scripts" />
-declare module 'nprogress';
-declare module 'react-copy-to-clipboard';
+// declare module 'nprogress';
+// declare module 'react-copy-to-clipboard';
 declare module 'react-pdf';
 declare module 'react-pdf/dist/esm/entry.webpack';
