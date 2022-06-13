9151df8c1d284104ebd6847913d483e6a0400360
lvyan <1376237693@qq.com>
2022-06-13 10:31:45 +0800

    移除animate.css改为cdn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import 'animate.css';
 import { useRoutes } from 'react-router-dom';
 import router from '@/router';
 import '@/styles/index.less';
--- src/views/expenses/index.tsx
+++ src/views/expenses/index.tsx
@@ -30,6 +30,7 @@ const Expenses = () => {
 
 	useEffect(() => {
 		getTabelData();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [pageSize, pageNum]);
 
 	const onFinish = () => {
