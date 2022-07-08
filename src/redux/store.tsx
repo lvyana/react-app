a6141449f8afc5b9e8b4ce11770e8656a89e408f
lvyan <1376237693@qq.com>
2022-07-08 19:12:34 +0800

    fix: 移除redux

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,15 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
-import { combineReducers, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import { composeWithDevTools } from 'redux-devtools-extension'; //开发者工具
 import layout from './reducers/layout';
 import user from './reducers/user';
 import log from './reducers/log';
-
-// const store = createStore(allreducer, composeWithDevTools(applyMiddleware(thunk, logger)));
-// export default store;
 
 const store = configureStore({
 	reducer: {
