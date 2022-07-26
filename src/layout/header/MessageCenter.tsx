e9e12af3f93381a45a1aaf4e88042f7855c76d3b
lvyan <1376237693@qq.com>
2022-07-26 11:14:42 +0800

    feat: 消息中心

--- src/layout/header/MessageCenter.tsx
+++ src/layout/header/MessageCenter.tsx
@@ -1,9 +1,9 @@
 /**
  * 消息中心
  */
-import React from 'react';
+import React, { useState } from 'react';
 import { BellOutlined } from '@ant-design/icons';
-import { Button, Col, Dropdown, Row, Tabs } from 'antd';
+import { Button, Col, Dropdown, MenuProps, Row, Tabs } from 'antd';
 import Icard from '@/components/iCard';
 import styles from './index.module.less';
 
@@ -16,33 +16,45 @@ const MessageCenter = () => {
 
 	const list = () => {
 		return (
-			<Icard className={`${styles['Layout-Tabs-center']} Box-Shadow`}>
+			<Icard className={`${styles['Layout-Tabs-center']} Box-Shadow`} style={{ padding: 0 }}>
 				<Tabs defaultActiveKey="1" onChange={onChange}>
-					<TabPane tab="通知" key="1">
+					<TabPane tab="通知" key="1" className={`${styles['Tabs-padding']}`}>
 						<div style={{ width: '100%' }}>Content of Tab Pane SEGSAFDSF阿斯顿发送到发送到阿斯顿发送到发送到</div>
 					</TabPane>
-					<TabPane tab="消息" key="2">
+					<TabPane tab="消息" key="2" className={`${styles['Tabs-padding']}`}>
 						Content of Tab Pane 2
 					</TabPane>
-					<TabPane tab="待办" key="3">
+					<TabPane tab="待办" key="3" className={`${styles['Tabs-padding']}`}>
 						Content of Tab Pane 3
 					</TabPane>
 				</Tabs>
 				<Row>
 					<Col span={12}>
-						<Button>Default Button</Button>
+						<Button style={{ width: '100%' }} className={`${styles['Tabs-btn-left']}`}>
+							全部已读
+						</Button>
 					</Col>
 					<Col span={12}>
-						<Button>Default Button</Button>
+						<Button style={{ width: '100%' }} className={`${styles['Tabs-btn-Right']}`}>
+							查看更多
+						</Button>
 					</Col>
 				</Row>
 			</Icard>
 		);
 	};
 
+	const [visible, setVisible] = useState(false);
+
+	const handleVisibleChange = (flag: boolean) => {
+		setVisible(flag);
+	};
+
 	return (
 		<>
 			<Dropdown
+				visible={visible}
+				onVisibleChange={handleVisibleChange}
 				overlay={list}
 				overlayClassName={`${styles['Layout-Dropdown']} ${styles['Layout-Dropdown-Widht']}`}
 				placement="bottomLeft"
