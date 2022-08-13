19e02ad0e5b18a5c2707e5a0c29f2b614e14142e
吕炎 <1376237693@qq.com>
2022-08-14 01:20:04 +0800

    feat: 图片懒加载

--- src/components/iLoading/index.tsx
+++ src/components/iLoading/index.tsx
@@ -19,16 +19,19 @@ interface Iprops {
 	children?: ReactNode | null;
 }
 const Loading: FC<Iprops> = ({ children, loading }) => {
-	// 判断 children 是否空对象
-	return typeof loading === 'boolean' ? (
-		<Spin indicator={antIcon} spinning={loading} delay={200}>
-			{children}
-		</Spin>
-	) : (
-		<div className={styles['Loading-Example']}>
-			<Spin indicator={antIcon} delay={200} style={{ backgroundColor: '#f0f2f5' }}></Spin>
-		</div>
-	);
+	if (typeof loading === 'boolean') {
+		return (
+			<Spin indicator={antIcon} spinning={loading} delay={200}>
+				{children}
+			</Spin>
+		);
+	} else {
+		return (
+			<div className={styles['Loading-Example']}>
+				<Spin indicator={antIcon} delay={200} style={{ backgroundColor: '#fff' }}></Spin>
+			</div>
+		);
+	}
 };
 
 export default Loading;
