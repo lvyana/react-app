7a732f9e8373ce5ce3bdca74bf45a2e802641b66
吕炎 <1376237693@qq.com>
2022-11-10 23:49:57 +0800

    style: tailwind 样式

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './tailwind.css';
 import { ConfigProvider } from 'antd';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import ErrorBoundary from '@/components/errorBoundary';
-import './App.css';
+
 import App from './App';
 // import 'antd/dist/antd.css';
 import './index.scss';
