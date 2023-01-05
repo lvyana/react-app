c0df732bc7b242185fa76a3a67a38132be679d16
吕炎 <1376237693@qq.com>
2023-01-05 22:19:06 +0800

    feat: 更换loading

--- src/router/suspenseLoad.tsx
+++ src/router/suspenseLoad.tsx
@@ -4,12 +4,13 @@
  *  @data 日期：2020年4月27日
  */
 import React, { ReactNode, Suspense } from 'react';
-import Loading from '@/antdComponents/iLoading';
+// import Loading from '@/antdComponents/iLoading';
+import Iloading from '@/pluginComponents/iLoading';
 // import Nprogress from './Nprogress';
 // 两种均可
 
 const SuspenseLoad = (element: ReactNode) => {
-	return <Suspense fallback={<Loading />}>{element}</Suspense>;
+	return <Suspense fallback={<Iloading />}>{element}</Suspense>;
 };
 
 export default SuspenseLoad;
