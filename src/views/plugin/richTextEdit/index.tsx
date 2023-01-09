fd5fbce178bdf700bafb4c48303c9fdb891fab7e
吕炎 <1376237693@qq.com>
2023-01-09 22:47:00 +0800

    feat: 项目优化

--- src/views/plugin/richTextEdit/index.tsx
+++ src/views/plugin/richTextEdit/index.tsx
@@ -23,12 +23,13 @@ export interface IMenuConfig {
 
 const RichTextEdit = () => {
 	const [editor, setEditor] = useState<IDomEditor | null>(null); // 存储 editor 实例
-	const defaultContent = [{ type: 'paragraph', children: [{ text: '一行文字' }] }];
+	const defaultHtml = `<h2>针对目前前端中台项目优化方案</h2><h3>1、代码管理&nbsp;git&nbsp;分支管理&nbsp;版本分支&nbsp;&nbsp;上线流程</h3><p><img src="https://project-1308388249.cos.ap-guangzhou.myqcloud.com/git.png" alt="git" data-href="https://project-1308388249.cos.ap-guangzhou.myqcloud.com/git.png" style="width: 785.00px;height: 622.20px;"/></p><h3>2、项目优化&nbsp;页面加载慢&nbsp;组件体验不好&nbsp;文件管理&nbsp;数据管理&nbsp;</h3><p>2.1、页面加载&nbsp;webpack打包优化，减少代码体积、静态文件放到cdn、路由懒加载、预加载</p><p>2.2、组件体验&nbsp;交互效果、组件性能调整</p><p>2.3、文件管理&nbsp;约定好文件名字、文件目录结构</p><p>2.4、针对项目使用较多的幂等数据存放vuex状态管理、较多的接口用mixin进行封装</p><h3>3、组件优化&nbsp;公共组件&nbsp;</h3><p>3.1、公共组件封装要减少使用负担</p><p>3.2、考虑公共组件封装使用场景，不必追求封装而封装</p><h3>4、代码优化</h3>`;
 	const toolbarConfig = {};
 	const editorConfig: Partial<IEditorConfig> = {
 		placeholder: '请输入内容...',
 		onCreated(editor: IDomEditor) {
 			setEditor(editor);
+			console.log(editor);
 		}, // 记录下 editor 实例，重要！
 		MENU_CONF: {}
 	};
@@ -73,7 +74,7 @@ const RichTextEdit = () => {
 	return (
 		<div className="animate__animated animate__fadeIn">
 			<Toolbar editor={editor} defaultConfig={toolbarConfig} mode="default" style={{ borderBottom: '1px solid #ccc' }} />
-			<Editor defaultConfig={editorConfig} defaultContent={defaultContent} mode="default" style={{ height: '500px' }} />
+			<Editor defaultConfig={editorConfig} defaultHtml={defaultHtml} mode="default" style={{ height: '500px' }} />
 			<div style={{ marginTop: '10px' }}>
 				<Button type="primary" onClick={onSubmit} style={{ marginRight: '5px' }}>
 					提交
