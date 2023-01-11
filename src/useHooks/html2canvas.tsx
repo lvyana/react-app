59856b4fa4ef58c6196cfef691d66de3a95733fd
lvyan <1376237693@qq.com>
2023-01-11 09:07:14 +0800

    feat: useHtml2canvas

---
+++ src/useHooks/html2canvas.tsx
@@ -0,0 +1,28 @@
+/**
+ * @name html2canvas下载图片
+ * @user ly
+ * @date 2023年1月10日
+ */
+import React from 'react';
+import html2canvas from 'html2canvas';
+
+const useHtml2canvas = (dom: HTMLElement) => {
+	const getImg = () => {
+		html2canvas(dom).then((canvas) => {
+			let url = canvas.toDataURL('image/png');
+			let aLink = document.createElement('a');
+			aLink.style.display = 'none';
+			aLink.href = url;
+			// console.log(aLink.href);
+			// router.push({name: 'menu', query: {'src': aLink.href}})
+			// aLink.download = "截图.png";
+			// document.body.appendChild(aLink);
+			// aLink.click();
+			// document.body.removeChild(aLink);
+			window.localStorage.setItem('src', aLink.href);
+		});
+	};
+	return getImg;
+};
+
+export default useHtml2canvas;
