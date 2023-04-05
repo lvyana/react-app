b23d855f3604012321f529e10dc683debf248293
吕炎 <1376237693@qq.com>
2023-04-05 16:27:40 +0800

    feat: EasyTyper

---
+++ src/views/plugin/easyTyper/index.tsx
@@ -0,0 +1,19 @@
+/**
+ * @file 模板
+ * @author ly
+ * @createDate
+ */
+import React from 'react';
+import useEasyTyper from '@/pluginComponents/easyTyper';
+import Icard from '@/antdComponents/iCard';
+
+// #----------- 上: ts类型定义 ----------- 分割线 ----------- 下: JS代码 -----------
+
+const EasyTyper = () => {
+	const { output } = useEasyTyper(
+		'asy-typer-js是一个轻量级的插件, 用于实现页面文字的打字机效果. 它使用起来非常简单, 只需要几行代码就能实现高大上的打字机效果.而且对MVVM框架支持完美，还兼容原生JS.'
+	);
+	return <Icard>{output}</Icard>;
+};
+
+export default EasyTyper;
