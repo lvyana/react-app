65a0d4cbd792db72ca09603bd8d1108b479bfd66
吕炎 <1376237693@qq.com>
2023-04-27 23:58:28 +0800

    feat: 修复面包屑

--- src/layout/header/crumb/index.tsx
+++ src/layout/header/crumb/index.tsx
@@ -10,6 +10,7 @@ import { useLocation, Link } from 'react-router-dom';
 import { Router, EXCLUDE_MENU } from '../../menuList/index';
 import { useAppSelector } from '@/store';
 import { GET_ROUTER } from '@/store/reducers/globalConfig';
+import { ItemType } from 'antd/es/breadcrumb/Breadcrumb';
 
 type MenuItem = Required<MenuProps>['items'][number];
 
@@ -20,25 +21,49 @@ const Crumb = () => {
 
 	const menuList = useAppSelector(GET_ROUTER);
 
-	const [currentRouter, SetcurrentRouter] = useState<Router[]>([]);
+	const [currentRouter, SetcurrentRouter] = useState<ItemType[]>([]);
 
 	useEffect(() => {
 		if (location.pathname.indexOf(EXCLUDE_MENU[0]) !== -1) return;
-		let routerArr: any[] = [];
+		let routerArr: Router[] = [];
 		location.pathname.split('/').map((item, i) => {
-			let is = menuList?.find((val) => {
-				console.log(val, item);
-
-				return val.path.indexOf(item) !== -1;
-			});
-
-			if (is) routerArr[i] = is;
+			if (i === 0) {
+				routerArr[0] = { path: '/', title: '系统', children: menuList };
+			} else {
+				let is = routerArr[i - 1]?.children?.find((val) => {
+					return val.path.indexOf(item) !== -1;
+				});
+				if (is) routerArr[i] = is;
+			}
 		});
 
-		SetcurrentRouter(routerArr);
+		SetcurrentRouter(getCrumbList(routerArr));
 	}, [location.pathname]);
+	console.log(currentRouter);
 
 	return <Breadcrumb items={currentRouter}></Breadcrumb>;
 };
 
 export default Crumb;
+
+const getCrumbList = (routerArr: Router[]) => {
+	return routerArr.map((item) => {
+		if (item.children) {
+			return {
+				title: item.title,
+				menu: {
+					items: item.children?.map((r) => {
+						return {
+							key: r.path,
+							label: <Link to={r.path}>{r.title}</Link>
+						};
+					})
+				}
+			};
+		}
+		return {
+			key: item.path,
+			title: item.title
+		};
+	});
+};
