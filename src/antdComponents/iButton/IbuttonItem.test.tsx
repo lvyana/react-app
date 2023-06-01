4893127a4849a5d648d68f95f3f89e98c7b0f6a0
lvyan <lvyan950824@163.com>
2023-06-01 22:08:24 +0800

    feat: 测试用例

--- src/antdComponents/iButton/IbuttonItem.test.tsx
+++ src/antdComponents/iButton/IbuttonItem.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, screen, queryHelpers } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import IbuttonItem from './IbuttonItem';
 import type { ButtonItemParams } from './type';
 
@@ -14,37 +14,37 @@ describe('<IbuttonItem />', () => {
 		block: false
 	};
 	it('组件是否正确地渲染了按钮文字', () => {
-		const { getByText } = render(<IbuttonItem buttonItem={mockButton} />);
-		expect(getByText('Click Me')).toBeInTheDocument();
+		render(<IbuttonItem buttonItem={mockButton} />);
+		expect(screen.getByText('Click Me')).toBeInTheDocument();
 	});
 	it('iconFont为string时 组件是否正确地渲染了icon', () => {
-		const { getByRole } = render(<IbuttonItem buttonItem={{ ...mockButton, iconFont: '12' }} />);
-		const icon = getByRole('img');
-		expect(icon).toHaveClass('anticon');
+		render(<IbuttonItem buttonItem={{ ...mockButton, iconFont: 'icon-font' }} />);
+		const icon = screen.getByRole('img');
 		const xlinkHref = icon.querySelector('svg use')?.getAttribute('xlink:href');
-		expect(xlinkHref).toBe('#12');
+		expect(xlinkHref).toBe('#icon-font');
 	});
 	it('iconFont为组件时 组件是否正确地渲染了icon', () => {
-		const { getByText } = render(<IbuttonItem buttonItem={{ ...mockButton, iconFont: <div>123</div> }} />);
-		const icon = getByText('123');
+		render(<IbuttonItem buttonItem={{ ...mockButton, iconFont: <div>123</div> }} />);
+		const icon = screen.getByText('123');
 		expect(icon).toBeInTheDocument();
 	});
+
 	it('点击按钮时是否会触发传入的 onClick 回调函数', () => {
-		const { getByText } = render(<IbuttonItem buttonItem={mockButton} onClick={mockOnClick} />);
-		const button = getByText('Click Me');
+		render(<IbuttonItem buttonItem={mockButton} onClick={mockOnClick} />);
+		const button = screen.getByText('Click Me');
 		fireEvent.click(button);
 		expect(mockOnClick).toHaveBeenCalledWith('primary', mockButton);
 	});
 	it('disabled 属性为 true 时，按钮是否被禁用', () => {
-		const { getByRole } = render(<IbuttonItem buttonItem={{ ...mockButton, disabled: true }} />);
-		const button = getByRole('button', { name: 'Click Me' });
+		render(<IbuttonItem buttonItem={{ ...mockButton, disabled: true }} />);
+		const button = screen.getByRole('button', { name: 'Click Me' });
 
 		expect(button).toBeDisabled();
 	});
 	it('加载状态为true时应该显示加载中的spinner', () => {
-		const { getByRole } = render(<IbuttonItem buttonItem={mockButton} loading={true} />);
+		render(<IbuttonItem buttonItem={mockButton} loading={true} />);
 
-		const loading = getByRole('img', { name: 'loading' });
+		const loading = screen.getByRole('img', { name: 'loading' });
 		expect(loading).toBeInTheDocument();
 	});
 });
--- src/antdComponents/iButton/index.test.tsx
+++ src/antdComponents/iButton/index.test.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import Ibutton from './index';
 import { ButtonItemParams } from './type';
 import { Provider } from 'react-redux';
 import store from '@/store';
 import matchMediaMock from 'match-media-mock';
+import type { MatchMediaMock } from 'match-media-mock';
+
+type Windows =
+	| {
+			matchMedia?: MatchMediaMock;
+	  }
+	| Window;
 
 describe('Ibutton component', () => {
 	const buttonList: ButtonItemParams<'submit' | 'cancel' | 'reset'>[] = [
@@ -12,36 +19,36 @@ describe('Ibutton component', () => {
 		{ type: 'cancel', name: '取消', span: 8, permission: 'permission2' },
 		{ type: 'reset', name: '重置', span: 8, permission: 'permission3' }
 	];
-	let matchMedia;
+
 	beforeAll(() => {
-		matchMedia = matchMediaMock.create();
-		window.matchMedia = matchMedia;
+		let matchMedia = matchMediaMock.create();
+		(window as Windows).matchMedia = matchMedia;
 	});
-	// afterAll(() => {
-	// 	window.matchMedia = undefined;
-	// });
+	afterAll(() => {
+		(window as Windows).matchMedia = undefined;
+	});
 
 	it('should render all buttons correctly', () => {
-		const { getByText, getAllByRole } = render(
+		render(
 			<Provider store={store}>
 				<Ibutton buttonList={buttonList} />
 			</Provider>
 		);
 		buttonList.forEach((button, i) => {
-			const items = getAllByRole('button');
+			const items = screen.getAllByRole('button');
 			expect(items[i]).toHaveTextContent(button.name.replace(/(.)(?!$)/g, '$1 '));
 		});
 	});
 	it('should call onClick handler when a button is clicked', () => {
 		const onClick = jest.fn();
-		const { getAllByRole } = render(
+		render(
 			<Provider store={store}>
 				<Ibutton buttonList={buttonList} onClick={onClick} />
 			</Provider>
 		);
 
 		buttonList.forEach((button, i) => {
-			const items = getAllByRole('button');
+			const items = screen.getAllByRole('button');
 			fireEvent.click(items[i]);
 			expect(onClick).toHaveBeenCalledWith(button.type, button);
 		});
