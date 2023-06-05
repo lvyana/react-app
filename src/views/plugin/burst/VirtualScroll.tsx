4eb3c95b59c49a6a0af8060234ec71fb89e7a41a
lvyan <lvyan950824@163.com>
2023-06-05 21:54:16 +0800

    feat: 数据切片

--- src/views/plugin/burst/VirtualScroll.tsx
+++ src/views/plugin/burst/VirtualScroll.tsx
@@ -3,27 +3,51 @@
  * @author ly
  * @createDate 2023年6月4日
  */
-import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import React, { useCallback, useMemo, useRef, useState } from 'react';
 
-interface VirtualScrollProps<T> {
+/**
+ * @param data 集合数据
+ * @param itemHeight 每一个组件高度
+ * @param visibleCount 窗口可见的组件数量
+ * @param visibleCount 预加载的组件数量
+ * @param key 唯一值
+ */
+type ConfigParam<T> = {
 	data: T[];
-	renderItem: (item: T) => React.ReactNode;
 	itemHeight: number;
 	visibleCount: number;
+	preloadCount?: number;
+	key: keyof T;
+};
+
+/**
+ * @param readerComponent 渲染的组件
+ * @param config 参数配置
+ */
+interface VirtualScrollProps<T> {
+	readerComponent: (item: T) => React.ReactNode;
+	config: ConfigParam<T>;
 }
 
 // #----------- 上: ts类型定义 ----------- 分割线 ----------- 下: JS代码 -----------
 
-const VirtualScroll = <T,>({ data, renderItem, itemHeight, visibleCount }: VirtualScrollProps<T>) => {
+const VirtualScroll = <T,>({ config, readerComponent }: VirtualScrollProps<T>) => {
+	const { data, itemHeight, visibleCount, key, preloadCount = 6 } = config;
+
+	// 本次渲染开始位置
 	const [start, setStart] = useState(0);
+
+	// 本次渲染结束位置
 	const [end, setEnd] = useState(visibleCount);
+
+	// 外层ref 用于获取滚动位置
 	const containerRef = useRef<HTMLDivElement>(null);
 
 	// 总高度 =  总数 * 一行高度
 	const totalHeight = useMemo(() => data.length * itemHeight, [data, itemHeight]);
 
-	// 可视高度 = (渲染总条数 - 预渲染条数) * 一行高度
-	const containerHeight = useMemo(() => (visibleCount - 6) * itemHeight, [visibleCount, itemHeight]);
+	// 可视高度 = 渲染总条数 * 一行高度
+	const containerHeight = useMemo(() => visibleCount * itemHeight, [visibleCount, itemHeight]);
 
 	// 滚动执行函数
 	const handleScroll = useCallback(() => {
@@ -38,10 +62,11 @@ const VirtualScroll = <T,>({ data, renderItem, itemHeight, visibleCount }: Virtu
 	return (
 		<div ref={containerRef} style={{ height: containerHeight, overflowY: 'auto' }} onScroll={handleScroll}>
 			<div style={{ height: totalHeight }}>
-				{data.slice(start, end).map((item, index) => {
+				{/* 渲染总条数 = 渲染数据 + 预渲染条数 */}
+				{data.slice(start, end + preloadCount).map((item) => {
 					return (
-						<div key={index} style={{ transform: `translateY(${start * itemHeight}px)` }}>
-							{renderItem(item)}
+						<div key={item[key] as React.Key} style={{ transform: `translateY(${start * itemHeight}px)` }}>
+							{readerComponent(item)}
 						</div>
 					);
 				})}
--- src/views/plugin/burst/index.tsx
+++ src/views/plugin/burst/index.tsx
@@ -3,12 +3,17 @@
  * @author ly
  * @createDate 2023年3月17日
  */
+import React, { useRef } from 'react';
 import { Button } from 'antd';
-import React, { FC, Fragment, ReactNode, memo, useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import Icard from '@/antdComponents/iCard';
 import VirtualScroll from './VirtualScroll';
 import scheduleSlicerHoc, { ScheduleSlicerRef } from './ScheduleSlicer';
 
+/**
+ * @param name 姓名
+ * @param age 年龄
+ * @param sex 性别
+ */
 type listParam = {
 	name: string;
 	age: number;
@@ -23,8 +28,10 @@ let arr: listParam[] = Array.from({ length: 40000 }, (v, k) => k).map((item) => 
 	};
 });
 
-const eachRenderNum = 400; // 每次渲染数量
+// 每次渲染数量
+const eachRenderNum = 400;
 
+// 初始化ScheduleSlicer组件
 const ScheduleSlicer = scheduleSlicerHoc<listParam>();
 
 // #----------- 上: ts类型定义 ----------- 分割线 ----------- 下: JS代码 -----------
@@ -52,14 +59,20 @@ const Burst = () => {
 				</Button>
 			</div>
 
-			<ScheduleSlicer ref={ScheduleSlicerRef} arr={arr} ReandList={ReandList} eachRenderNum={eachRenderNum}></ScheduleSlicer>
+			<ScheduleSlicer
+				ref={ScheduleSlicerRef}
+				config={{ arr, eachRenderNum, key: 'age' }}
+				readerComponent={readerComponent}></ScheduleSlicer>
 			{/* 性能要求还是得要虚拟加载 */}
-			<VirtualScroll<listParam> data={arr} renderItem={ReandList} itemHeight={50} visibleCount={15} />
+			<VirtualScroll<listParam>
+				config={{ data: arr, itemHeight: 50, visibleCount: 10, key: 'age', preloadCount: 2 }}
+				readerComponent={readerComponent}
+			/>
 		</Icard>
 	);
 };
 
-const ReandList = ({ name, age, sex }: listParam) => {
+const readerComponent = ({ name, age, sex }: listParam) => {
 	return (
 		<div style={{ height: 50, border: '1px solid blue' }}>
 			<span> 姓名: {name}</span>
@@ -68,6 +81,6 @@ const ReandList = ({ name, age, sex }: listParam) => {
 		</div>
 	);
 };
-ReandList.displayName = 'ReandList';
+readerComponent.displayName = 'readerComponent';
 
 export default Burst;
