8de6b4e24a0d8420d354a5dada4e3da9a11699dc
lvyan <1376237693@qq.com>
2023-06-07 11:39:38 +0800

    test: 测试

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -10,26 +10,16 @@ module.exports = {
 				message: 'Expected class selector to be kebab-case'
 			}
 		],
-		'string-quotes': 'single', // 单引号
 		'color-function-notation': 'legacy',
 		'at-rule-empty-line-before': null,
 		'at-rule-no-unknown': null,
-		'at-rule-name-case': 'lower', // 指定@规则名的大小写
 		'length-zero-no-unit': true, // 禁止零长度的单位（可自动修复）
 		'shorthand-property-no-redundant-values': true, // 简写属性
-		'number-leading-zero': 'never', // 小数不带0
 		'declaration-block-no-duplicate-properties': true, // 禁止声明快重复属性
 		'no-descending-specificity': true, // 禁止在具有较高优先级的选择器后出现被其覆盖的较低优先级的选择器。
 		'selector-max-id': 0, // 限制一个选择器中 ID 选择器的数量
 		'max-nesting-depth': 3,
 		'no-invalid-double-slash-comments': true, //不允许双斜杠注释(/ /…)不支持CSS
-		indentation: [
-			2,
-			{
-				// 指定缩进  warning 提醒
-				severity: 'warning'
-			}
-		],
 		'order/properties-order': [
 			// 规则顺序
 			'position',
