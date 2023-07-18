0c071d64640d14c7d98b263415c27e4416e906ae
lvyan <1376237693@qq.com>
2023-07-18 16:30:28 +0800

    chore: stylelint配置

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -10,22 +10,16 @@ module.exports = {
 				message: 'Expected class selector to be kebab-case' // 提示信息
 			}
 		],
-		'string-quotes': 'single', // 字符串使用单引号
 		'color-function-notation': 'legacy', // 颜色函数使用legacy
 		'at-rule-empty-line-before': null, // 不检查@规则前是否有空行
 		'at-rule-no-unknown': null, // 忽略未知的@规则
-		'at-rule-name-case': 'lower', // @规则名使用小写
 		'length-zero-no-unit': true, // 禁止零长度的单位（可自动修复）
 		'shorthand-property-no-redundant-values': true, // 简写属性
-		'number-leading-zero': 'never', // 小数不带0
 		'declaration-block-no-duplicate-properties': true, // 禁止声明快重复属性
 		'no-descending-specificity': true, // 禁止在具有较高优先级的选择器后出现被其覆盖的较低优先级的选择器。
 		'selector-max-id': 0, // 限制一个选择器中 ID 选择器的数量
 		'max-nesting-depth': 3, // 最大嵌套深度为3
 		'no-invalid-double-slash-comments': true, // 不允许双斜杠注释(/ /…)不支持CSS
-		'no-missing-end-of-source-newline': true, // 文件末尾需要一个空行
-		indentation: 2, // 缩进使用两个空格
-		'selector-max-empty-lines': 0, // 选择器后不允许有空行
 		'order/properties-order': [
 			// 检查属性顺序是否符合规范
 			'position',
